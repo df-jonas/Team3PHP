f8ac06089e631328a4d6340dfbf414e9c5ee7c88
unknown <ludovic.marchand@student.ehb.be>
2016-10-26 18:37:57 +0200

    Staff API af voor testing

--- resources/views/test.blade.php
+++ resources/views/test.blade.php
@@ -26,24 +26,102 @@
 
     .content {
     }
+
+    .title {
+        font-style: italic;
+        font-size: 2em;
+        font-color: green;
+    }
+
+    form {
+        margin-top: 25px;
+        text-align: justify;
+    }
+
+    form div {
+        width: 10em;
+        margin: 10px 0;
+    }
 </style>
 <body>
 <div class="container">
     <div class="content">
-        <div class="title">test 5</div>
 
-        <form method="POST" action="/staff">
+        <div class="title">ayyyyyylmao</div>
+
+       <!-- <div class="title">Create new staff member</div>
+
+        <form method="POST" action="/staff/create">
             {!! csrf_field() !!}
 
             <div>
-                <label for="id">ID: </label>
-                <input type="text" name="id" value="25">
+                <label for="AddressID">AddressID: </label>
+                <input type="text" name="AddressID" value="1">
+            </div>
+            <div>
+                <label for="StationID">StationID: </label>
+                <input type="text" name="StationID" value="1">
+            </div>
+            <div>
+                <label for="FirstName">FirstName: </label>
+                <input type="text" name="FirstName" value="test_">
+            </div>
+            <div>
+                <label for="LastName">LastName: </label>
+                <input type="text" name="LastName" value="test_">
+            </div>
+            <div>
+                <label for="UserName">UserName: </label>
+                <input type="text" name="UserName" value="test_">
+            </div>
+            <div>
+                <label for="Password">Password: </label>
+                <input type="text" name="Password" value="test_">
+            </div>
+            <div>
+                <label for="Rights">Rights: </label>
+                <input type="text" name="Rights" value="0">
+            </div>
+            <div>
+                <label for="BirthDate">BirthDate: </label>
+                <input type="text" name="BirthDate" value="2011-12-12">
+            </div>
+            <div>
+                <label for="Email">Email: </label>
+                <input type="text" name="Email" value="test@test.test">
             </div>
 
             <div>
                 <button type="submit">Submit</button>
             </div>
         </form>
+
+        <div class="title">Delete id 5</div>
+
+        <form method="POST" action="/staff/delete/5">
+            <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+            <input type="hidden" name="_method" value="DELETE">
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+
+        <div class="title">Update id 4</div>
+
+        <form method="POST" action="/staff/update/4">
+
+            <div>
+                <label for="test">test: </label>
+                <input type="text" name="test" value="pompompom">
+            </div>
+
+
+            <div>
+                <input type="hidden" name="_token" value="<?php echo csrf_token(); ?>">
+                <input type="hidden" name="_method" value="PUT">
+                <button type="submit">Submit</button>
+            </div>
+        </form> -->
 
     </div>
 </div>
