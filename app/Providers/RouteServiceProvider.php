0523d509c5478b6cf9860e231787b437eb15ff9b
unknown <ludovic.marchand@student.ehb.be>
2016-11-01 14:34:50 +0100

    trying to solve fork problems

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -71,7 +71,7 @@ class RouteServiceProvider extends ServiceProvider
         Route::group([
             'middleware' => 'api',
             'namespace' => $this->namespace,
-            'prefix' => 'api',
+            'prefix' => '/~ludovic.marchand/DigX/SP/public/api',
         ], function ($router) {
             require base_path('routes/api.php');
         });
