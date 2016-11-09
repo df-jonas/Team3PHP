a6d1e06e1e7fa79de1f4609547df424e9f2266fc
unknown <ludovic.marchand@student.ehb.be>
2016-11-09 22:53:39 +0100

    Customer + route; Api re-ograniseren;

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,28 @@ use Illuminate\Http\Request;
 //    return $request->user();
 //})->middleware('auth:api');
 
+Route::group(['prefix' => 'address', 'middleware' => 'auth:api'], function()
+{
+    Route::get('/{id}', 'AddressController@byID');
+
+    Route::post('create', 'AddressController@create');
+
+    Route::Put('update/{id}', 'AddressController@update');
+    Route::Delete('delete/{id}', 'AddressController@delete');
+});
+
+Route::group(['prefix' => 'station', 'middleware' => 'auth:api'], function()
+{
+    Route::get('/', 'StationController@index');
+    Route::get('/{id}', 'StationController@byID');
+
+    Route::post('create', 'StationController@create');
+    Route::post('createWithAddress', 'StationController@createWithAddress');
+
+    Route::Put('update/{id}', 'StationController@update');
+    Route::Delete('delete/{id}', 'StationController@delete');
+});
+
 Route::group(['prefix' => 'staff'], function()
 {
     Route::post('login', 'UserController@login');
@@ -34,14 +56,45 @@ Route::group(['prefix' => 'staff'], function()
     });
 });
 
-Route::group(['prefix' => 'address', 'middleware' => 'auth:api'], function()
+Route::group(['prefix' => 'customer', 'middleware' => 'auth:api'], function()
 {
-    Route::get('/{id}', 'AddressController@byID');
+    Route::get('/', 'CustomerController@index');
+    Route::get('/{id}', 'CustomerController@byID');
 
-    Route::post('create', 'AddressController@create');
+    Route::post('create', 'CustomerController@create');
+    Route::post('/createWithAddress', 'CustomerController@createWithAddress');
 
-    Route::Put('update/{id}', 'AddressController@update');
-    Route::Delete('delete/{id}', 'AddressController@delete');
+    Route::Put('update/{id}', 'CustomerController@update');
+});
+@todo;
+// Rotue
+
+Route::group(['prefix' => 'subscription', 'middleware' => 'auth:api'], function()
+{
+//    Route::get('/', 'StationController@index');
+//    Route::get('/{id}', 'StationController@byID');
+
+//    Route::post('create', 'StationController@create');
+    Route::post('createWithRoute', 'SubscriptionController@createWithRoute');
+
+//    Route::Put('update/{id}', 'StationController@update');
+//    Route::Delete('delete/{id}', 'StationController@delete');
+});
+
+@todo;
+// Discount
+// RailCard
+// Line
+
+Route::group(['prefix' => 'ticket', 'middleware' => 'auth:api'], function()
+{
+    Route::get('/', 'TicketController@index');
+    Route::get('/{id}', 'TicketController@byID');
+
+    Route::post('create', 'TicketController@create');
+
+    Route::Put('update/{id}', 'TicketController@update');
+    Route::Delete('delete/{id}', 'TicketController@delete');
 });
 
 Route::group(['prefix' => 'lostObject', 'middleware' => 'auth:api'], function()
@@ -55,40 +108,6 @@ Route::group(['prefix' => 'lostObject', 'middleware' => 'auth:api'], function()
     Route::Delete('delete/{id}', 'LostObjectController@delete');
 });
 
-Route::group(['prefix' => 'ticket', 'middleware' => 'auth:api'], function()
-{
-    Route::get('/', 'TicketController@index');
-    Route::get('/{id}', 'TicketController@byID');
-
-    Route::post('create', 'TicketController@create');
-
-    Route::Put('update/{id}', 'TicketController@update');
-    Route::Delete('delete/{id}', 'TicketController@delete');
-});
-
-Route::group(['prefix' => 'station', 'middleware' => 'auth:api'], function()
-{
-    Route::get('/', 'StationController@index');
-    Route::get('/{id}', 'StationController@byID');
-
-    Route::post('create', 'StationController@create');
-    Route::post('createWithAddress', 'StationController@createWithAddress');
-
-    Route::Put('update/{id}', 'StationController@update');
-    Route::Delete('delete/{id}', 'StationController@delete');
-});
-
-Route::group(['prefix' => 'subscription', 'middleware' => 'auth:api'], function()
-{
-//    Route::get('/', 'StationController@index');
-//    Route::get('/{id}', 'StationController@byID');
-
-//    Route::post('create', 'StationController@create');
-    Route::post('createWithRoute', 'SubscriptionController@createWithRoute');
-
-//    Route::Put('update/{id}', 'StationController@update');
-//    Route::Delete('delete/{id}', 'StationController@delete');
-});
 
 
 Route::group(['prefix' => 'test', 'middleware' => 'auth:api'], function()
